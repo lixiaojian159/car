e59e5b65aad11a5f840505ac9cfb82f0b5d55811
lijian <852688838@qq.com>
2019-01-09 21:40:41 +0800

    20190109

--- application/admin/controller/Car.php
+++ application/admin/controller/Car.php
@@ -4,9 +4,19 @@ namespace app\admin\controller;
 
 use think\Controller;
 
+use app\admin\model\Cate;
+
 class Car extends Controller{
 
+    //叉车列表
 	public function index(){
-		return '叉车管理';
+		return $this->view->fetch();
+	}
+
+	public function add(){
+		//叉车分类
+		$cates = Cate::where('pid',1)->select();
+		$this->view->assign('cates',$cates);
+		return $this->view->fetch();
 	}
 }
